bed419dd56f1197ae3a52037f21a36b524d4efb2
NielsDeBacker01 <niels.debacker01@student.ap.be>
2024-08-17 18:40:38 +0200

    clean class

--- KaijuDemolition/Assets/Scripts/UI/HP.cs
+++ KaijuDemolition/Assets/Scripts/UI/HP.cs
@@ -1,10 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class HP : MonoBehaviour
 {
