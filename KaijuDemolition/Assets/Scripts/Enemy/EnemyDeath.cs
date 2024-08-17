5e3aa162be65be994d11d446fc6a879dd5663cd6
NielsDeBacker01 <niels.debacker01@student.ap.be>
2024-08-18 01:39:33 +0200

    add boss enemies

--- KaijuDemolition/Assets/Scripts/Enemy/EnemyDeath.cs
+++ KaijuDemolition/Assets/Scripts/Enemy/EnemyDeath.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyDeath : MonoBehaviour
 {
     public GameObject exp;
     private Enemy enemy;
+    [SerializeField] private bool boss = false;
     void Start()
     {
         enemy = GetComponent<Enemy>();
@@ -17,6 +19,18 @@ public class EnemyDeath : MonoBehaviour
         {
             if (enemy.HP <= 0)
             {
+                if(boss)
+                {
+                    if(SceneManager.GetActiveScene().name == "Stage1City")
+                    {
+                        SceneManager.LoadScene("Stage2World");
+                    }
+                    if(SceneManager.GetActiveScene().name == "Stage2World")
+                    {
+                        SceneManager.LoadScene("Stage3Space");
+                    }
+                    
+                }
                 Instantiate(exp, new Vector3(this.gameObject.transform.localPosition.x, this.gameObject.transform.localPosition.y, this.gameObject.transform.localPosition.z), Quaternion.identity);
                 Destroy(this.gameObject);
             }
