d7c1d9494014ed739578ff69069299a36d4bf662
NielsDeBacker01 <niels.debacker01@student.ap.be>
2024-08-17 15:05:49 +0200

    aim attack

---
+++ KaijuDemolition/Assets/Scripts/Player/AimAttack.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class AimAttack : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f; 
+        Vector3 direction = mousePosition - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+}
