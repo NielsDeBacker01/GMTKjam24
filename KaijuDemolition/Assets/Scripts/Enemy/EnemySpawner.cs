b0d0dc78f530e6d9da7a9f8a52dc7aeff26b73b6
NielsDeBacker01 <niels.debacker01@student.ap.be>
2024-08-18 03:47:21 +0200

    bosses & scaling spawns

--- KaijuDemolition/Assets/Scripts/Enemy/EnemySpawner.cs
+++ KaijuDemolition/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -4,29 +4,71 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject theEnemy;
+    public GameObject enemy1;
+    public GameObject boss;
     [SerializeField] private float radius = 5f;
     [SerializeField] private float cooldown = 1.5f;
     [SerializeField] private Transform player;
+    private float timerValue = 0;
+    private bool bossSpawned;
 
     private void Start()
     {
+        timerValue = 0;
         StartCoroutine(SpawnEnemies());
+        bossSpawned = false;
     }
 
     private IEnumerator SpawnEnemies()
     {
         while (true)
         {
-            float randomAngle = UnityEngine.Random.Range(0f, Mathf.PI * 2);
-            Vector3 spawnPosition = new Vector3(
-                Mathf.Cos(randomAngle) * (float)(radius * Math.Sqrt(player.lossyScale.x )),    
-                Mathf.Sin(randomAngle) * (float)(radius * Math.Sqrt(player.lossyScale.x )),    
-                0f                                  
-            );
-            spawnPosition += this.gameObject.transform.position;
-            Instantiate(theEnemy, spawnPosition, Quaternion.identity);
+            if(timerValue < 50)
+            {
+                for (int i = (int)Math.Ceiling(timerValue/10); i > 0; i--)
+                {
+                    spawnEnemy();
+                }
+            }
+            else if(timerValue > 50 && 100 > timerValue)
+            {
+                if(!bossSpawned)
+                {
+                    spawnBoss();
+                    bossSpawned = true;
+                }
+            }
+
             yield return new WaitForSeconds(cooldown);
+            Debug.Log(timerValue);
         }
+    }
+
+    private void FixedUpdate() {
+        timerValue += Time.deltaTime;
+    }
+
+    private void spawnEnemy()
+    {
+        float randomAngle = UnityEngine.Random.Range(0f, Mathf.PI * 2);
+        Vector3 spawnPosition = new Vector3(
+            Mathf.Cos(randomAngle) * (float)(radius * Math.Sqrt(player.lossyScale.x )),    
+            Mathf.Sin(randomAngle) * (float)(radius * Math.Sqrt(player.lossyScale.x )),    
+            0f                                  
+        );
+        spawnPosition += this.gameObject.transform.position;
+        Instantiate(enemy1, spawnPosition, Quaternion.identity);
+    }
+
+    private void spawnBoss()
+    {
+        float randomAngle = UnityEngine.Random.Range(0f, Mathf.PI * 2);
+        Vector3 spawnPosition = new Vector3(
+            Mathf.Cos(randomAngle) * (float)(radius * Math.Sqrt(player.lossyScale.x ) * 2),    
+            Mathf.Sin(randomAngle) * (float)(radius * Math.Sqrt(player.lossyScale.x ) * 2),    
+            0f                                  
+        );
+        spawnPosition += this.gameObject.transform.position;
+        Instantiate(boss, spawnPosition, Quaternion.identity);
     }
 }
